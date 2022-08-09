d8db79f4e11afb698967f0b1eaa6563f6046e737
jkimdev <jkim.dev@icloud.com>
2022-08-09 15:09:41 +0900

    split string result to insert

--- src/main/kotlin/com/jimmy/degreesbatch/service/PerformanceService.kt
+++ src/main/kotlin/com/jimmy/degreesbatch/service/PerformanceService.kt
@@ -1,7 +1,6 @@
 package com.jimmy.degreesbatch.service
 
-import com.jimmy.degreesbatch.Mapper.ActorMapper
-import com.jimmy.degreesbatch.Mapper.PerformanceMapper
+import com.jimmy.degreesbatch.Mapper.*
 import com.jimmy.degreesbatch.Model.PerformanceDetailDto
 import com.jimmy.degreesbatch.Model.PerformanceDto
 import lombok.RequiredArgsConstructor
@@ -15,6 +14,14 @@ class PerformanceService {
     lateinit var mapper: PerformanceMapper
     @Autowired
     lateinit var actorMapper: ActorMapper
+    @Autowired
+    lateinit var crewMapper: CrewMapper
+    @Autowired
+    lateinit var priceMapper: PriceMapper
+    @Autowired
+    lateinit var producerMapper: ProducerMapper
+    @Autowired
+    lateinit var scheduleMapper: ScheduleMapper
 
     // SELECT
     fun selectPerformance(): List<PerformanceDto> {
@@ -28,10 +35,28 @@ class PerformanceService {
     // INSERT
     fun insertPerformance(vo: List<PerformanceDetailDto>) {
         for (i in vo.indices) {
-            var actors = vo[i].prfcast.split(',')
+            val actors = vo[i].prfcast.split(',')
+            val crews = vo[i].prfcrew.split(',')
+            val prices = vo[i].pcseguidance.split(", ")
+            val producers = vo[i].entrpsnm.split(',')
+            val schedules = vo[i].dtguidance.split(", ")
+
             for (name in actors) {
                 actorMapper.insertActor(vo[i].mt20id, name)
             }
+            for (crew in crews) {
+                crewMapper.insertCrew(vo[i].mt20id, crew)
+            }
+            for (price in prices) {
+                priceMapper.insertPrice(vo[i].mt20id, price)
+            }
+            for (producer in producers) {
+                producerMapper.insertProducer(vo[i].mt20id, producer)
+            }
+            for (schedule in schedules) {
+                scheduleMapper.insertSchedule(vo[i].mt20id, schedule)
+            }
+
             mapper.insertPerformance(vo[i])
         }
     }
